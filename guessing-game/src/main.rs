81d294b94c0b799a909ac15580231b79df65ad9b
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-06-01 16:17:44 +0500

    Guessing game in rust

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,10 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering::{Equal, Greater, Less};
 use std::io::stdin;
 
 fn main() {
-    println!("Please enter your guess number: ");
-    let mut guess = String::new();
-    stdin()
-        .read_line(&mut guess)
-        .expect("I guess this is an error");
-    println!("Your guess {guess}");
+    println!("Please enter your guess number.");
+    let secret_number = rand::rng().random_range(1..=100);
+    loop {
+        let mut guess = String::new();
+        stdin()
+            .read_line(&mut guess)
+            .expect("I guess this is an error");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Type a number!");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Less => println!("It is low"),
+            Equal => {
+                println!("You won!");
+                break;
+            }
+            Greater => println!("It is high"),
+        }
+    }
 }
